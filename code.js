c85517c3757e60d4b9fb24f0e2813d1ca847cee7
Eduardo <144747945+eddchinea@users.noreply.github.com>
2024-01-18 18:50:33 -0500

    feat: create funcs to populate display with btns

--- code.js
+++ code.js
@@ -15,7 +15,34 @@ function divide(a, b) {
     return a / b;
 }
 
-//Operate using an operator
+// Calculate using operator
 function operate(operator, num1, num2) {
     return operator(num1, num2)
 }
+
+// Display
+const display = document.querySelector('.display');
+let displayValue = '';
+display.textContent = displayValue;
+
+// Numbers
+const numbers = document.querySelectorAll('.btn-number');
+numbers.forEach(number => number.addEventListener('click', () => {
+    displayValue += number.textContent;
+    display.textContent = displayValue;
+}))
+
+// Operators
+const operators = document.querySelectorAll('.btn-operator');
+operators.forEach(operator => operator.addEventListener('click', () => {
+    displayValue += ` ${operator.textContent} `;
+    display.textContent = displayValue;
+}))
+
+// Clear
+const clear = document.querySelector('.btn-clear');
+clear.addEventListener('click', () => {
+    displayValue = '';
+    display.textContent = displayValue;
+})
+
